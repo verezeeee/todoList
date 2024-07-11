fc5ec980a39ad8ad24e78ec5d84219f857eb8570
verezeeee <theusma58@gmail.com>
2024-07-11 10:11:48 -0300

    added userDefaults to save and persist the data

---
+++ TodoList/Views/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  TodoList
+//
+//  Created by Matheus on 11/07/24.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
